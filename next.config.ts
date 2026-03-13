e9629a2487532231ef702517b94041adc68b0b74
Anyon-Aida <kovacszalan23@gmail.com>
2026-03-13 19:39:18 +0100

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,20 @@
-import { image } from "framer-motion/client";
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  image: { unoptimized: true },
-  eslint: { ignoreDuringBuilds: true },
+const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async rewrites() {
-    return [{ source: '/works/:slug', destination: '/projects/:slug/index.html' }];
-  }
+    return [
+      {
+        source: "/works/:slug",
+        destination: "/projects/:slug",
+      },
+    ];
+  },
 };
-module.exports = nextConfig;
 
-/** 
- * 
-   rm -rf .next .turbo out
-   npm run build
-   npm run build && npx next export
-   https://www.linkedin.com/company/digital-activision
-   https://www.instagram.com/digital_activision/
-*/
+export default nextConfig;
